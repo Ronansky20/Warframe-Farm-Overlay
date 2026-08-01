287db9434db36b4c84922e097f105a72a0108e39
ronansky <ronandrost20@gmail.com>
2026-08-01 02:21:41 +0200

    Created first tests

---
+++ Core.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Tests
+{
+    public class PlannerTests
+    {
+        [Fact]
+        public void AskingForABaseMaterial_TellsYouToFarmThatMaterial()
+        {
+            var plan = Planner.Plan("Neurodes", 5);
+
+            Assert.Equal(5, plan["Neurodes"]);
+        }
+    }
+}
---
+++ Core/Planner.cs
@@ -0,0 +1,12 @@
+﻿namespace Core
+{
+    public class Planner
+    {
+        public static Dictionary<string, int> Plan(string target, int quantity)
+        {
+            var result = new Dictionary<string, int>();
+            result[target] = quantity;
+            return result;
+        }
+    }
+}
