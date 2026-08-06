1eaa0f41881c0418fc4ffc2a17c4e4607bb8b185
ronansky <ronandrost20@gmail.com>
2026-08-06 04:35:46 +0200

    Add console app with WFCD fetcher

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+
+string url = "https://raw.githubusercontent.com/WFCD/warframe-items/master/data/json/Warframes.json";
+
+using var http = new HttpClient();
+
+string json = await http.GetStringAsync(url);
+
+Console.WriteLine($"Got {json.Length} characters of JSON.");
+Console.WriteLine(json.Substring(0, 500));
