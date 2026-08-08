9c5cffd99d7aaa5376740a88279e2bd6190989e9
ronansky <ronandrost20@gmail.com>
2026-08-08 20:43:03 +0200

    Fetch and merge weapons and frames

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System.Net.Http;
 using Core;
 
-string url = "https://raw.githubusercontent.com/WFCD/warframe-items/master/data/json/Warframes.json";
+string[] urls =
+{
+    "https://raw.githubusercontent.com/WFCD/warframe-items/master/data/json/Warframes.json",
+    "https://raw.githubusercontent.com/WFCD/warframe-items/master/data/json/Primary.json",
+    "https://raw.githubusercontent.com/WFCD/warframe-items/master/data/json/Secondary.json",
+    "https://raw.githubusercontent.com/WFCD/warframe-items/master/data/json/Melee.json",
+};
 
 using var http = new HttpClient();
 
-Console.WriteLine("Fetching Warframe data...");
-string json = await http.GetStringAsync(url);
+var recipes = new Dictionary<string, Recipe>();
 
-var recipes = WarframeDataAdapter.Parse(json);
-Console.WriteLine($"Loaded {recipes.Count} warframe recipes.");
+foreach (string url in urls)
+{
+    Console.WriteLine($"Fetching {url.Split('/').Last()}...");
+    string json = await http.GetStringAsync(url);
+
+    var parsed = WarframeDataAdapter.Parse(json);
+    foreach (var entry in parsed)
+    {
+        recipes[entry.Key] = entry.Value; // add/merge into the combined set
+    }
+}
+
+Console.WriteLine($"Loaded {recipes.Count} recipes total.");
 
 // Build a flat lookup: material name → its best drop location.
 var locations = new Dictionary<string, DropLocation>();
@@ -22,7 +38,7 @@ foreach (var recipe in recipes.Values)
     }
 }
 
-Console.Write("Which warframe do you want to farm? ");
+Console.Write("What do you want to farm? ");
 string target = Console.ReadLine() ?? "";
 
 var inventory = new Dictionary<string, int>();
